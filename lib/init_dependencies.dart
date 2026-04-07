324db509af7bfbac5d862a52aee29b9079cf6fe8
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-07 14:18:18 +0300

    Add dependency injection setup for Firebase and authentication services

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
+import 'package:skillswap/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:skillswap/features/auth/domain/repositories/auth_repository.dart';
+import 'package:skillswap/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:skillswap/features/auth/presentation/cubits/auth_cubit.dart';
+import 'package:skillswap/firebase_options.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  final firebaseApp = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  serviceLocator.registerLazySingleton(() => FirebaseAuth.instance);
+  serviceLocator.registerLazySingleton(() => FirebaseFirestore.instance);
+
+  _initAuth();
+}
+
+void _initAuth() {
+  // Repository
+  serviceLocator.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(serviceLocator()),
+  );
+
+  // Usecases
+  serviceLocator.registerFactory(
+    () => UserSignUp(serviceLocator()),
+  );
+
+  // Cubits
+  serviceLocator.registerLazySingleton(
+    () => AuthCubit(userSignUp: serviceLocator()),
+  );
+}
