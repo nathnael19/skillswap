6a42eed2211700f58020a3bffaa6844a63be1938
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-07 14:18:48 +0300

    Add AuthRepositoryImpl implementation for Firebase authentication, including methods for sign-up, login, and logout with error handling

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:skillswap/core/error/failures.dart';
+import 'package:skillswap/features/auth/domain/repositories/auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final FirebaseAuth _firebaseAuth;
+
+  const AuthRepositoryImpl(this._firebaseAuth);
+
+  @override
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (credential.user == null) {
+        return left(ServerFailure('User is null!'));
+      }
+      return right(credential.user!.uid);
+    } on FirebaseAuthException catch (e) {
+      return left(ServerFailure(e.message ?? 'Unknown error.'));
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, String>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (credential.user == null) {
+        return left(ServerFailure('User is null!'));
+      }
+      return right(credential.user!.uid);
+    } on FirebaseAuthException catch (e) {
+      return left(ServerFailure(e.message ?? 'Unknown error.'));
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> logout() async {
+    try {
+      await _firebaseAuth.signOut();
+      return right(null);
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
+  }
+}
