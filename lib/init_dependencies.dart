8ed89f70adf2801988edd1bc09288f2f08903430
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-11 16:02:54 +0300

    Refactor dependency initialization in init_dependencies.dart; add http client and ApiClient registration, implement Home and Chat module initialization with respective repositories and cubits, enhancing modularity and network handling in the application.

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:skillswap/core/network/api_client.dart';
 import 'package:skillswap/core/storage/firebase_storage_service.dart';
 import 'package:skillswap/core/storage/storage_service.dart';
 import 'package:skillswap/features/auth/data/repositories/auth_repository_impl.dart';
@@ -12,30 +14,52 @@ import 'package:skillswap/features/auth/domain/usecases/user_sign_in.dart';
 import 'package:skillswap/features/auth/domain/usecases/user_sign_out.dart';
 import 'package:skillswap/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:skillswap/features/auth/presentation/cubits/auth_cubit.dart';
+import 'package:skillswap/features/home/data/repositories/home_repository_impl.dart';
+import 'package:skillswap/features/home/domain/repositories/home_repository.dart';
+import 'package:skillswap/features/home/presentation/cubits/profile_cubit.dart';
+import 'package:skillswap/features/home/presentation/cubits/credits_cubit.dart';
+import 'package:skillswap/features/home/presentation/cubits/discovery_cubit.dart';
+import 'package:skillswap/features/home/presentation/cubits/matches_cubit.dart';
+import 'package:skillswap/features/home/domain/repositories/chat_repository.dart';
+import 'package:skillswap/features/home/data/repositories/chat_repository_impl.dart';
+import 'package:skillswap/features/home/presentation/cubits/chat_cubit.dart';
 import 'package:skillswap/firebase_options.dart';
 
 final serviceLocator = GetIt.instance;
 
 Future<void> initDependencies() async {
-  final firebaseApp = await Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   serviceLocator.registerLazySingleton(() => FirebaseAuth.instance);
   serviceLocator.registerLazySingleton(() => FirebaseFirestore.instance);
   serviceLocator.registerLazySingleton(() => FirebaseStorage.instance);
+  serviceLocator.registerLazySingleton(() => http.Client());
+
+  serviceLocator.registerLazySingleton(
+    () => ApiClient(
+      serviceLocator<FirebaseAuth>(),
+      serviceLocator<http.Client>(),
+    ),
+  );
 
   serviceLocator.registerLazySingleton<StorageService>(
     () => FirebaseStorageService(serviceLocator()),
   );
 
   _initAuth();
+  _initHome();
+  _initChat();
 }
 
 void _initAuth() {
   // Repository
   serviceLocator.registerLazySingleton<AuthRepository>(
-    () => AuthRepositoryImpl(serviceLocator()),
+    () => AuthRepositoryImpl(
+      serviceLocator<FirebaseAuth>(),
+      serviceLocator<ApiClient>(),
+    ),
   );
 
   // Usecases
@@ -60,5 +84,38 @@ void _initAuth() {
       getCurrentUser: serviceLocator(),
       userSignOut: serviceLocator(),
     ),
+  );
+}
+
+void _initHome() {
+  // Repository
+  serviceLocator.registerLazySingleton<HomeRepository>(
+    () => HomeRepositoryImpl(serviceLocator<ApiClient>()),
+  );
+
+  // Cubits
+  serviceLocator.registerFactory(
+    () => DiscoveryCubit(serviceLocator<HomeRepository>()),
+  );
+  serviceLocator.registerFactory(
+    () => MatchesCubit(serviceLocator<HomeRepository>()),
+  );
+  serviceLocator.registerFactory(
+    () => CreditsCubit(serviceLocator<HomeRepository>()),
+  );
+  serviceLocator.registerFactory(
+    () => ProfileCubit(serviceLocator<HomeRepository>()),
+  );
+}
+
+void _initChat() {
+  // Repository
+  serviceLocator.registerLazySingleton<ChatRepository>(
+    () => ChatRepositoryImpl(serviceLocator<ApiClient>()),
+  );
+
+  // Cubits
+  serviceLocator.registerFactory(
+    () => ChatCubit(chatRepository: serviceLocator<ChatRepository>()),
   );
 }
