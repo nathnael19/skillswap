cf3a2abf8fb4386386c362dae3f9960f08d7528b
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-11 17:04:38 +0300

    Add new methods to HomeRepository for fetching received likes and user by ID, enhancing data retrieval capabilities.

--- lib/features/home/domain/repositories/home_repository.dart
+++ lib/features/home/domain/repositories/home_repository.dart
@@ -5,11 +5,13 @@ import 'package:skillswap/features/home/domain/models/user_model.dart';
 abstract interface class HomeRepository {
   Future<Either<Failure, List<User>>> getDiscoveryUsers({String? category});
   Future<Either<Failure, List<User>>> getMatches();
+  Future<Either<Failure, List<User>>> getLikesReceived();
   Future<Either<Failure, Map<String, dynamic>>> getCredits();
   Future<Either<Failure, bool>> swipeUser({
     required String targetId,
     required String direction,
   });
   Future<Either<Failure, User>> getMe();
+  Future<Either<Failure, User>> getUserById(String userId);
   Future<Either<Failure, User>> updateUserProfile(User user);
 }
