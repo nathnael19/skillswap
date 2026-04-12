f4118da929e5091d1d500dc28fe725ebb0a9e467
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-13 00:12:38 +0300

    Add DiscoveryState classes for state management in discovery feature
    Introduce DiscoveryState and its subclasses to manage the state of the discovery feature. This includes states for initial loading, loaded data, filtered results, and error handling, enhancing the overall state management structure.

---
+++ lib/features/home/presentation/cubits/discovery_state.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+import 'package:skillswap/features/home/domain/models/user_model.dart';
+
+abstract class DiscoveryState extends Equatable {
+  const DiscoveryState();
+  @override
+  List<Object?> get props => [];
+}
+
+class DiscoveryInitial extends DiscoveryState {}
+class DiscoveryLoading extends DiscoveryState {}
+
+class DiscoveryLoaded extends DiscoveryState {
+  final List<User> allUsers;
+  final List<User> users;
+  
+  const DiscoveryLoaded({required this.allUsers, required this.users});
+  
+  @override
+  List<Object?> get props => [allUsers, users];
+}
+
+class DiscoveryFiltered extends DiscoveryLoaded {
+  const DiscoveryFiltered({required super.allUsers, required super.users});
+}
+
+class DiscoveryError extends DiscoveryState {
+  final String message;
+  const DiscoveryError(this.message);
+  @override
+  List<Object?> get props => [message];
+}
