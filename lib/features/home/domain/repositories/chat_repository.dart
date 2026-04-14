2a96bfc117c12169c480100a1bfd2636225aec63
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-14 11:20:04 +0300

    feat: update ChatRepository to support dynamic message streams and add signaling message functionality

--- lib/features/home/domain/repositories/chat_repository.dart
+++ lib/features/home/domain/repositories/chat_repository.dart
@@ -4,9 +4,10 @@ import 'package:skillswap/features/home/domain/models/message_model.dart';
 
 abstract interface class ChatRepository {
   Future<Either<Failure, List<Message>>> getMessages(String matchId);
-  Stream<Message> getMessagesStream(String matchId);
+  Stream<dynamic> getMessagesStream(String matchId);
   Future<Either<Failure, Message>> sendMessage({
     required String matchId,
     required String content,
   });
+  void sendSignalingMessage(Map<String, dynamic> payload);
 }
