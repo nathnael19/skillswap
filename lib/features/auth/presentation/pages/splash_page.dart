abb00b8af8b8ba3c112597e4fad54aa6750d0a90
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-17 12:57:04 +0300

    refactor: correct import path for HomePage to match updated directory structure

--- lib/features/auth/presentation/pages/splash_page.dart
+++ lib/features/auth/presentation/pages/splash_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:skillswap/features/auth/presentation/cubits/auth_cubit.dart';
 import 'package:skillswap/features/auth/presentation/cubits/auth_state.dart';
-import 'package:skillswap/features/home/presentation/pages/home_page.dart';
+import 'package:skillswap/features/home/presentation/pages/home/home_page.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
