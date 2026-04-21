afa7e81940156145e48c2f41ca958305815c7bdb
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-21 19:43:01 +0300

    feat: remove signaling listener and related code from ChatRepositoryImpl for cleaner implementation

--- lib/features/home/data/repositories/chat_repository_impl.dart
+++ lib/features/home/data/repositories/chat_repository_impl.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:skillswap/core/error/failures.dart';
@@ -13,33 +12,11 @@ class ChatRepositoryImpl implements ChatRepository {
   final ApiClient _apiClient;
   final FirebaseDatabase _db;
 
-  // Global stream for all incoming RTDB events (Messages & Signaling)
+  // Global stream for all incoming RTDB events (Messages)
   final StreamController<dynamic> _globalStreamController = StreamController<dynamic>.broadcast();
-  
-  StreamSubscription? _signalingSubscription;
   final Map<String, List<StreamSubscription>> _matchSubscriptions = {};
-  String? _currentUserUid;
 
-  ChatRepositoryImpl(this._apiClient, this._db) {
-    _initSignalingListener();
-  }
-
-  void _initSignalingListener() {
-    _currentUserUid = FirebaseAuth.instance.currentUser?.uid;
-    if (_currentUserUid == null) return;
-
-    _signalingSubscription?.cancel();
-    // Listen for signaling events addressed to me
-    _signalingSubscription = _db.ref('signaling/$_currentUserUid').onChildAdded.listen((event) {
-      final data = event.snapshot.value;
-      if (data is Map) {
-        final Map<String, dynamic> signalingData = Map<String, dynamic>.from(data);
-        _globalStreamController.add(signalingData);
-        // Remove signal after processing to keep RTDB clean (one-time signals)
-        event.snapshot.ref.remove();
-      }
-    });
-  }
+  ChatRepositoryImpl(this._apiClient, this._db);
 
   @override
   Future<Either<Failure, List<Message>>> getMessages(String matchId) async {
@@ -57,11 +34,6 @@ class ChatRepositoryImpl implements ChatRepository {
 
   @override
   Stream<dynamic> getMessagesStream(String matchId) {
-    // Re-init signaling listener if it wasn't set up at construction time
-    if (_signalingSubscription == null) {
-      _initSignalingListener();
-    }
-
     // Check if we are already listening to this match
     if (!_matchSubscriptions.containsKey(matchId)) {
       final nodeRef = _db.ref('messages/$matchId');
@@ -86,13 +58,10 @@ class ChatRepositoryImpl implements ChatRepository {
       _matchSubscriptions[matchId] = [addedSub, changedSub];
     }
     
-    // Return a stream that filters for this specific match or WebRTC signaling
+    // Return a stream that filters for this specific match only
     return _globalStreamController.stream.where((event) {
       if (event is Message) {
         return event.matchId == matchId;
-      }
-      if (event is Map<String, dynamic> && event['type'] == 'webrtc_signaling') {
-        return true; 
       }
       return false;
     });
@@ -133,19 +102,7 @@ class ChatRepositoryImpl implements ChatRepository {
     }
   }
 
-  @override
-  void sendSignalingMessage(Map<String, dynamic> payload) {
-    final targetUid = payload['target_uid'];
-    if (targetUid == null) return;
-
-    final senderUid = FirebaseAuth.instance.currentUser?.uid;
-    payload['sender_id'] = senderUid;
-
-    _db.ref('signaling/$targetUid').push().set(payload);
-  }
-
   void dispose() {
-    _signalingSubscription?.cancel();
     for (var subs in _matchSubscriptions.values) {
       for (var s in subs) {
         s.cancel();
