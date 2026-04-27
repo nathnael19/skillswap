dfeba610f3bf829fc25ea96af28cfa95b1323277
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-27 16:25:11 +0300

    feat: integrate flutter_dotenv for environment variable management and update pubspec.yaml and .gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,19 @@ import 'package:skillswap/core/common/widgets/offline_toast.dart';
 
 import 'package:skillswap/core/services/notification_service.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorageDirectory.web
