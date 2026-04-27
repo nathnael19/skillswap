19250a2dcddcd95563ae09d5f991723a6646db84
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-27 15:43:49 +0300

    feat: add AppBreakpoints class for responsive design breakpoints to enhance layout adaptability

---
+++ lib/core/layout/breakpoints.dart
@@ -0,0 +1,6 @@
+class AppBreakpoints {
+  static const double compact = 360.0;
+  static const double tablet = 600.0;
+  static const double tabletWide = 840.0;
+  static const double desktop = 1200.0;
+}
